c07a806337941f96621331319c87227dcfa10ff2
archeshshrivas2206 <archeshshrivas14@gmail.com>
2026-09-11 12:05:47 +0530

    one more

---
+++ display_labels.py
@@ -0,0 +1,27 @@
+
+
+PRODUCT_DISPLAY_LABELS = {
+    "identifier": "Product ID",
+    "productName":"Product name",
+    "productInformation": "Product description",
+    "unitType": "Unit type",
+    "hsnSacCd": "HSN Code",
+    "startDateTime":"Start date",
+    "endDateTime": "End date",
+    "categoryName": "Product category",
+}
+
+PRICE_DISPLAY_LABELS = {
+    "identifier": "Price ID",
+    "productOfferingIdentifier": "Product ID",
+    "priceName": "Price name",
+    "taxSchemeCd": "Default tax applicability",
+}
+
+CHARACTERISTICS_DISPLAY_LABELS = {
+    "identifier": "Characteristic ID",
+    "productOfferingIdentifier": "Product ID",
+    "name": "Field name",
+    "valueType": "Field type",
+    "value": "Default value",
+}
